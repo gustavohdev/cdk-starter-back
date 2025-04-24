02f01313f57c0f4851a5777bd4a2717ea9ac8063
Gustavo Avide <g_a93@outlook.com>
2025-04-24 16:13:27 +0100

    feat: add authstack and prettierrc

---
+++ src/infra/stacks/AuthStack.ts
@@ -0,0 +1,48 @@
+import * as cdk from 'aws-cdk-lib'
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs'
+import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
+
+export class AuthStack extends cdk.Stack {
+
+    public readonly spacesTable: ITable 
+    private userPool: UserPool
+    private userPoolClient: UserPoolClient
+    constructor(scope: Construct, id: string, props?:cdk.StackProps ){
+        super(scope, id, props);
+
+        this.createUserPool()
+        this.createUserPoolCLient()
+    }
+    
+    private createUserPool(){
+        this.userPool = new UserPool(this, 'SpaceUserPool', {
+            selfSignUpEnabled: true,
+            signInAliases: {
+                phone: true,
+                username: true,
+            },
+        
+    });
+
+    new cdk.CfnOutput(this, 'SpaceUserPoolId', {
+        value: this.userPool.userPoolId,
+    })
+}
+
+    private createUserPoolCLient(){
+        this.userPoolClient = this.userPool.addClient( 'SpaceUserPoolClient', {
+            authFlows: {
+                adminUserPassword: true,
+                custom: true,
+                userPassword: true,
+                userSrp: true,
+            },
+        });
+
+        new cdk.CfnOutput(this, 'SpaceUserPoolClientId', {
+            value: this.userPoolClient.userPoolClientId,
+        });
+
+    }
+}
